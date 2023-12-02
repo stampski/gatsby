1e2a5d19b9581c6f95975f6e2f3ab60b0d37eb42
stampski <stamp.portal@gmail.com>
2023-12-02 03:24:17 +0100

    Gatsby ETAP2

--- stampski-gatsby/gatsby-config.js
+++ stampski-gatsby/gatsby-config.js
@@ -3,7 +3,25 @@
  */
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `StaMPski GatsbyJS`,
+    siteUrl: `https://stampski.000webhostapp.com`
   },
-  plugins: [],
-}
+  plugins: [{
+    resolve: 'gatsby-source-wordpress',
+    options: {
+      "url": "https://stampski.000webhostapp.com/graphql"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-theme-ui", /*"gatsby-plugin-google-gtag",*/ "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
