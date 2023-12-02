c16c3f0d5313ad4035517a06ffc794464ed35920
stampski <stamp.portal@gmail.com>
2023-12-02 04:10:06 +0100

    wirus

--- stampski-gatsby/gatsby-config.js
+++ stampski-gatsby/gatsby-config.js
@@ -9,7 +9,25 @@ module.exports = {
   plugins: [{
     resolve: 'gatsby-source-wordpress',
     options: {
-      "url": "https://stampski.000webhostapp.com/graphql"
+      "url": "https://stampski.000webhostapp.com/graphql",
+      schema: {
+        //Prefixes all WP Types with "Wp" so "Post and allPost" become "WpPost and allWpPost".
+        typePrefix: `Wp`,
+      },
+      develop: {
+        //caches media files outside of Gatsby's default cache an thus allows them to persist through a cache reset.
+        hardCacheMediaFiles: true,
+      },
+      type: {
+        Post: {
+          limit:
+            process.env.NODE_ENV === `development`
+              ? // Lets just pull 50 posts in development to make it easy on ourselves (aka. faster).
+                50
+              : // and we don't actually need more than 5000 in production for this particular site
+                5000,
+        },
+      },
     }
   }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-theme-ui", /*"gatsby-plugin-google-gtag",*/ "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
